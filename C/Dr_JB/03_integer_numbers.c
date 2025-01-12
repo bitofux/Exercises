e93aade0879f4adedf2a561ff112fc937a4b04b0
bitofux <wsccie@icloud.com>
2025-01-12 23:07:26 +0800

    chore(C/Dr_JB/): added 03_integer_numbers.c 04_float.c 05_while.c

---
+++ C/Dr_JB/03_integer_numbers.c
@@ -0,0 +1,15 @@
+/* integer_numbers.cc */
+#include <stdio.h>
+
+int main () {
+
+  int x,y,anwser;
+  printf("Enter a number: \n");
+  scanf("%d",&x);
+  printf("Enter a another number: \n");
+  scanf("%d",&y);
+  anwser = x + y;
+  printf("anwser: %d\n",anwser);
+
+  return 0;
+}
