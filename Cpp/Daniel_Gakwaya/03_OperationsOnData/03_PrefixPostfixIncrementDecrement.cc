000a6f4968e0aeff79517e6b31a48c0508a5fb46
bitofux <wsccie@icloud.com>
2025-01-17 00:03:13 +0800

    chore(Cpp/Daniel_Gakwaya/03_OperationsOnData/): update 03_PrefixPostfixIncrementDecrement.cc

--- Cpp/Daniel_Gakwaya/03_OperationsOnData/03_PrefixPostfixIncrementDecrement.cc
+++ Cpp/Daniel_Gakwaya/03_OperationsOnData/03_PrefixPostfixIncrementDecrement.cc
@@ -1,38 +1,39 @@
 #include <iostream>
 
-int main () {
+int main() {
 
-  unsigned int value  = 5;
-
-  //Increment by one
+  unsigned int value{5};
+  // Increment by one
   value += 1;
   std::cout << "The value is : " << value << std::endl;
 
-  value = 5;//Reset value to 5
+  value = 5; // Reset value to 5
 
-  //Decrement by one
+  // Decrement by one
   value -= 1;
   std::cout << "The value is : " << value << std::endl;
 
   std::cout << "=====Postfix increment and decrement=====" << std::endl;
-  //reset value to 5
+  // reset value to 5
   value = 5;
 
-  std::cout << "The value is (increment) : " << value++ << std::endl;//right value
+  std::cout << "The value is (increment) : " << value++
+            << std::endl; // right value
   std::cout << "The value is : " << value << std::endl;
 
   std::cout << std::endl;
-  //Decrement with Postfix
-  value = 5;//reset value to 5
+  // Decrement with Postfix
+  value = 5; // reset value to 5
 
   std::cout << "The value is (decrement) : " << value-- << std::endl;
   std::cout << "The value is : " << value << std::endl;
 
   std::cout << "=====prefix increment and decrement=====" << std::endl;
-  value = 5;//Reset value to 5
-  std::cout << "The value is (increment) : " << ++value << std::endl;//left value
+  value = 5; // Reset value to 5
+  std::cout << "The value is (increment) : " << ++value
+            << std::endl; // left value
   std::cout << "The value is : " << value << std::endl;
-  value = 5;//Reset value to 5
+  value = 5; // Reset value to 5
   std::cout << "The value is : " << --value << std::endl;
 
   return 0;
