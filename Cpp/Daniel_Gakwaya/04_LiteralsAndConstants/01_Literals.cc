4648e4fc66c691f73c5bc7290aaa0ef325791031
bitofux <wsccie@icloud.com>
2025-01-26 21:56:52 +0800

    chore(Cpp/Daniel_Gakwaya/04_LiteralsAndConstants/): added 01_Literals.cc

--- Cpp/Daniel_Gakwaya/04_LiteralsAndConstants/01_Literals.cc
+++ Cpp/Daniel_Gakwaya/04_LiteralsAndConstants/01_Literals.cc
@@ -16,5 +16,25 @@ int main () {
   signed short int signed_short_int {-456}; // no special literal type for short
   unsigned short int unsigned_short_int {5678U};
 
+  // 4 bytes
+  const int int_var {55};
+  signed int signed_int_var {66};
+  signed signed_var {77};
+  unsigned int unsigned_int {555u};
+
+  // 4 or 8 bytes
+  long long_var {88l};
+  long int long_int {33l};
+  signed long signed_long {44l};
+  signed long int signed_long_int {44l};
+  unsigned long int unsigned_long_int {555ul};
+
+  long long long_long_var {888ll};
+  long long int long_long_int_var {888ll};
+  signed long long signed_long_long_var {444ll};
+  signed long long int signed_long_long_int_var {555ll};
+  unsigned long long int unsigned_long_long_int_var {555ull};
+
+
   return 0;
 }
