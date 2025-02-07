dcc194f39f6bef2bfa20ed6cfe9dfb335be936f4
bitofux <wsccie@icloud.com>
2025-02-07 22:54:26 +0800

    chore(~/Exercises/Makefile/): added 01/ 02/ 03/

---
+++ Makefile/01/main.c
@@ -0,0 +1,6 @@
+#include "test.h"
+
+int main (){
+  add();
+  return 0;
+}
---
+++ Makefile/01/test.c
@@ -0,0 +1,7 @@
+#include "test.h"
+#include <stdio.h>
+
+void add(){
+  printf("%s\n",__FUNCTION__);
+  printf("%s\n",__FILE__);
+}
