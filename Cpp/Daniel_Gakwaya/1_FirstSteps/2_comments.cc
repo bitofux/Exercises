6e56baa301f4a4c2c43851f6621ba1c32fdfdfc0
bitofux <wsccie@icloud.com>
2025-03-27 23:26:20 +0800

    chore(~/Exercises/Cpp/Daniel_Gakwaya/1_FirstSteps/): added Makefile 1_HelloWorld.cc 2_comments.cc

---
+++ Cpp/Daniel_Gakwaya/1_FirstSteps/2_comments.cc
@@ -0,0 +1,16 @@
+//单行注释
+
+//预处理指令引入iostream头文件
+#include <iostream>
+
+/*
+包含多个文本行的块注释
+*/
+
+int main () {
+  
+  //打印"Hello World!"到控制台
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+  //程序在此处结束
+}
