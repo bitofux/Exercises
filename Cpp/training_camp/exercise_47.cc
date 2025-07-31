d53f6e3bc69ce4fbeb5886af859d11519d14a842
bitofux <wsccie@icloud.com>
2025-07-31 09:11:17 +0800

    update

---
+++ Cpp/training_camp/exercise_47.cc
@@ -0,0 +1,32 @@
+// 编写一个程序，使用malloc动态分配一个整型数组的空间，允许用户输入数组的大小(不超过100)，然后输入数组的元素值，并打印数组的//内容，最后使用free释放内存
+
+#include <iostream>
+
+int main() {
+    std::cout << "输入数组的大小(不超过100): ";
+    int size = 0;
+    std::cin >> size;
+
+    std::cout << "size = " << size << std::endl;
+    // 使用malloc分配size大小的整型数组
+    int* ptr = reinterpret_cast<int*>(malloc(size * sizeof(*ptr)));
+    if (ptr == NULL) {
+        std::cerr << "分配内存失败\n";
+        return 1;
+    }
+
+    // 用户输入数组元素的值
+    std::cout << "输入各个元素的值(按元素顺序输入)\n";
+    for (int i = 0; i < size; i++) {
+        std::cout << "输入第" << i + 1 << "个元素的值: ";
+        std::cin >> ptr[i];
+    }
+
+    // 打印数组中元素的值
+    for (int i = 0; i < size; i++) {
+        std::cout << ptr[i] << ' ';
+    }
+    std::cout << '\n';
+
+    return 0;
+}
---
+++ Cpp/training_camp/exercise_48.cc
@@ -0,0 +1,36 @@
+// 编写一个程序,首先使用malloc分配一个整型数组的空间，并填充一些初始值。然后使用realloc增加数组的大小
+// 并再次填充新分配的部分，最后打印出整个数组的内容，最后使用free释放内存
+
+#include <cstdlib>
+#include <iostream>
+
+int main() {
+    int* ptr = reinterpret_cast<int*>(malloc(sizeof(*ptr) * 5));
+    if (!ptr) {
+        std::cerr << "分配内存失败\n";
+        return 1;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        ptr[i] = i * 2;
+    }
+
+    int* tmp = reinterpret_cast<int*>(realloc(ptr, sizeof(*tmp) * 10));
+    if (tmp == NULL) {
+        free(ptr);
+        ptr = nullptr;
+    } else {
+        ptr = tmp;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        ptr[i + 5] = ptr[i] * 2;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << ptr[i] << ' ';
+    }
+    std::cout << '\n';
+
+    return 0;
+}
