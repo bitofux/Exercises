fd0fee4e31663bf63bcc74c51c369a8ae44cf420
bitofux <wsccie@icloud.com>
2026-09-14 23:06:19 +0800

    update

--- data_structure/sl/4.SortingAlgorithm/insertion_sort.cc
+++ data_structure/sl/4.SortingAlgorithm/insertion_sort.cc
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <random>
 
-void insertion_sort(int arr[], int size) {
+void insertion_sort_one(int arr[], int size) {
     for (int i = 0; i < size - 1; ++i) {
         int val = arr[i + 1];
         int j = i;
@@ -16,9 +16,51 @@ void insertion_sort(int arr[], int size) {
             if (arr[j] <= val) {
                 break;
             }
+        }
+        // 判断j + 1与i是否相等，若相等则不需要移动和插入数据
+        if (j + 1 > i) {
+            continue;
+        }
+
+        // 从后一个元素开始移动数据
+        for (int k = i; k >= j + 1; --k) {
+            arr[k + 1] = arr[k];
+        }
+
+        // 插入数据
+        arr[j + 1] = val;
+    }
+}
+void insertion_sort_two(int arr[], int size) {
+    for (int i = 0; i < size - 1; ++i) {
+        int val = arr[i + 1];
+        int j = i;
+        for (; j >= 0; --j) {
+            if (arr[j] <= val) {
+                break;
+            }
+            // 移动数据
             arr[j + 1] = arr[j];
         }
 
+        // 插入数据
+        arr[j + 1] = val;
+    }
+}
+
+void insertion_sort_three(int arr[], int size) {
+    for (int i = 1; i < size; ++i) {
+        int val = arr[i];
+        int j = i - 1;
+        for (; j >= 0; --j) {
+            if (arr[j] <= val) {
+                break;
+            }
+            // 移动数据
+            arr[j + 1] = arr[j];
+        }
+
+        // 插入数据
         arr[j + 1] = val;
     }
 }
@@ -38,7 +80,7 @@ int main() {
     }
     std::cout << "\n";
 
-    insertion_sort(arr, sizeof(arr) / sizeof(arr[0]));
+    insertion_sort_three(arr, sizeof(arr) / sizeof(arr[0]));
 
     for (int ele : arr) {
         std::cout << ele << " ";
